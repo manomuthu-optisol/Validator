a071c8704811ad72f60ef24930315ef68b5a7396
Muthukumar <muthukumar.m@optisolbusiness.com>
2024-03-24 19:59:17 +0530

    Added Framework

---
+++ ValidFW/ValidFW/Valid.swift
@@ -0,0 +1,12 @@
+//
+//  Valid.swift
+//  ValidFW
+//
+//  Created by MAC-OBS-20 on 24/03/24.
+//
+
+import Foundation
+
+public func validator(){
+    print("Say hello!!")
+}
---
+++ ValidFW/ValidFW/ValidFW.h
@@ -0,0 +1,18 @@
+//
+//  ValidFW.h
+//  ValidFW
+//
+//  Created by MAC-OBS-20 on 24/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ValidFW.
+FOUNDATION_EXPORT double ValidFWVersionNumber;
+
+//! Project version string for ValidFW.
+FOUNDATION_EXPORT const unsigned char ValidFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ValidFW/PublicHeader.h>
+
+
